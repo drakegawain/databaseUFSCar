395913b0f1f0ca578683522987c261fe7a06f492
thales bolotti <thales.souzalopes@gmail.com>
2022-04-07 17:15:13 -0300

    added insert_buys

--- Python/main.py
+++ Python/main.py
@@ -16,8 +16,14 @@ def main():
        return ''.join(random.choice(string.ascii_letters) for x in range(number_of_letters))
 
     def random_date(start=None, end=None):
-        start_date = datetime.date(2003, 1, 1)
-        end_date = datetime.date(2022, 2, 1)
+        if start is not None and end is not None:
+            start=start.split(',')
+            end=end.split(',')
+            start_date=datetime.date(year=int(start[0]), day=int(start[1]), month=int(start[2]))
+            end_date=datetime.date(year=int(end[0]),day=int(end[1]),month=int(end[2]))
+        else:
+            start_date = datetime.date(2003, 1, 1)
+            end_date = datetime.date(2022, 2, 1)
         time_between_dates = end_date - start_date
         days_between_dates = time_between_dates.days
         random_number_of_days = random.randrange(days_between_dates)
@@ -39,9 +45,32 @@ def main():
             cursor.execute("INSERT INTO dbo.Usuarios (CPF, Nome, Usuario, DataNasc, DataRegistro, Email, Senha) VALUES ({cpf},'{Nome}','{Usuario}','{DataNasc}','{DataRegistro}','{Email}','{Senha}')".format(cpf=CPF, Nome=NAME, Usuario=USUARIO, DataNasc=DATANASC, DataRegistro=DATAREGISTRO, Email=EMAIL, Senha=SENHA))
             cursor.commit()
         return
-    insert_users()
 
-    cursor.execute('SELECT * FROM dbo.Usuarios')
+    def insert_buys(cursor=cursor, number_of_buys=20, date_start=None, date_end=None):
+        for buy in range(number_of_buys):
+            if date_start != None and date_end != None:
+                date=random_date(date_start, date_end)
+            else:
+                raise Exception
+            CODPRODUTO=random.randint(1,20)
+            QTD=random.randint(1,5)
+            METODO=['Pay Pal', 'Pix', 'Boleto bancario', 'Cartao de credito']
+            METODO_RANDOM=random.randint(0,3)
+            ARRAY_OF_CPF=cursor.execute("SELECT CPF FROM dbo.Usuarios").fetchall()
+            FINAL_ARRAY=["None"]
+            for INDEX in range(len(ARRAY_OF_CPF)):
+                NEW_ARRAY_OF_CPF=ARRAY_OF_CPF[INDEX]
+                FINAL_ARRAY.append(NEW_ARRAY_OF_CPF[0])
+            FINAL_ARRAY.remove("None")
+            CPF_RANDOM=random.randint(0, len(FINAL_ARRAY))
+            cursor.execute("INSERT INTO dbo.Compras (CodProduto, Qtd, CPF, Metodo, DataCompra) VALUES ({CodProduto},{Qtd},{CPF},'{Metodo}','{DataCompra}')".format(CodProduto=CODPRODUTO, Qtd=QTD, CPF=FINAL_ARRAY[CPF_RANDOM], Metodo=METODO[METODO_RANDOM], DataCompra=date))
+            cursor.commit()
+        return
+
+    insert_buys(date_start='2021,1,01', date_end='2021,28,05')
+    #insert_users()
+
+    cursor.execute('SELECT * FROM dbo.Compras')
  
     for i in cursor:
         print(i)
